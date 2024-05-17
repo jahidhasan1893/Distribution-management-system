deec427762662740f85bdb2e3fc8d538f1e8ff8c
Md Jahid Hasan <jahidhasan1893@gmail.com>
2024-05-17 11:35:46 +0600

    update

--- db.js
+++ db.js
@@ -1,10 +1,15 @@
+require('dotenv').config();
+
 const mysql = require('mysql');
 
-const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'password',
-  database: 'your_database_name'
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
-module.exports = pool;
+connection.connect((err) => {
+    if (err) console.log('Database server is down');
+    console.log('Connected to MySQL database');
+});
