3cbe1fa442127f8c520b38522747f64ccc2ce925
Md Jahid Hasan <jahidhasan1893@gmail.com>
2024-06-19 10:54:19 +0600

    resolved issues and refactored

---
+++ config/db.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+
+connection.connect((err) => {
+    if (err) console.log('Database server is down');
+    else console.log('Connected to MySQL database');
+});
